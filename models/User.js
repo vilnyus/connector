689f799d1a1ebd0ba83bd9e332873f4a5e2190e9
vilnyus <govagharsh@gmail.com>
2019-07-23 18:43:10 +0400

    auth login register protect

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// Create Schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  pasword: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
